1768bda612c8f3baaaf6be1111eda4047e5e4198
Jamie Hewland <jhewland@gmail.com>
2018-01-05 12:50:45 +0200

    Make Rubocop happy

--- lib/facter/certbot_live_certs.rb
+++ lib/facter/certbot_live_certs.rb
@@ -1,17 +1,16 @@
 # List the files in the 'live' path. Note that this only works for the default
 # 'live' path: /etc/letsencrypt/live
 def contains_certs?(live_dir)
-  return false if !File.directory?(live_dir)
+  return false unless File.directory?(live_dir)
 
   ['cert.pem', 'chain.pem', 'fullchain.pem', 'privkey.pem'].all? do |f|
-    File.file?(File.join(live_dir, 'fullchain.pem'))
+    File.file?(File.join(live_dir, f))
   end
 end
 
 Facter.add(:certbot_live_certs) do
   setcode do
-    Dir.glob('/etc/letsencrypt/live/*').select do
-      |f| contains_certs?(f)
-    end.map { |f| File.basename(f) }
+    dirs = Dir.glob('/etc/letsencrypt/live/*').select { |f| contains_certs?(f) }
+    dirs.map { |f| File.basename(f) }
   end
 end
